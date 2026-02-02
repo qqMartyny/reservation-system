e49789104e1d6f37a0142ebf652561d90754faeb
martyny <qqmartyny@gmail.com>
2026-02-02 22:49:31 +0300

    feat(service): add service

---
+++ src/main/java/com/reserv/reservation_system/ReservationService.java
@@ -0,0 +1,12 @@
+package com.reserv.reservation_system;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationService {
+
+    public String getReservationById() {
+        return "it works";    
+    }
+    
+}
