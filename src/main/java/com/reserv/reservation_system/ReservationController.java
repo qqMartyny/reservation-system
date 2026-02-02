357d3d14c9c33bbd2c4818716d9d2f5e7eb73a3b
martyny <qqmartyny@gmail.com>
2026-02-02 23:23:43 +0300

    refactor: make /id handle catch id

--- src/main/java/com/reserv/reservation_system/ReservationController.java
+++ src/main/java/com/reserv/reservation_system/ReservationController.java
@@ -1,6 +1,7 @@
 package com.reserv.reservation_system;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +13,9 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
-    @GetMapping
-    public String getReservationById() {
-        return reservationService.getReservationById();
+    @GetMapping("/{id}")
+    public Reservation getReservationById(@PathVariable("id") Long id) {
+
+        return reservationService.getReservationById(id);
     }
 }
