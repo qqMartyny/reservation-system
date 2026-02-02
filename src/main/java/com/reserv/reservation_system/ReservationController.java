0369e5dc58e62276dc90c87407322158b3cfecea
martyny <qqmartyny@gmail.com>
2026-02-03 00:19:48 +0300

    feat(controller): add basic logging for /{id} and /getAll

--- src/main/java/com/reserv/reservation_system/ReservationController.java
+++ src/main/java/com/reserv/reservation_system/ReservationController.java
@@ -2,6 +2,8 @@ package com.reserv.reservation_system;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ReservationController {
    
+    private static final Logger log = LoggerFactory.getLogger(ReservationController.class);
+
+
     private final ReservationService reservationService;
 
     public ReservationController(ReservationService reservationService) {
@@ -17,12 +22,14 @@ public class ReservationController {
 
     @GetMapping("/{id}")
     public Reservation getReservationById(@PathVariable("id") Long id) {
-
+        
+        log.info("Called getReservationById() with id " + id);
         return reservationService.getReservationById(id);
     }
 
     @GetMapping("/getAll")
     public List<Reservation> getAllReservations() {
+        log.info("Called getAllReservations()");
         return reservationService.findAllReservations();
     }
 }
