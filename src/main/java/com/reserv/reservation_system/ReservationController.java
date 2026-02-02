65295ce51539fe414159c503a568f4750fe9e5c3
martyny <qqmartyny@gmail.com>
2026-02-03 02:19:33 +0300

    refactor(controller): add requestMapping and change handlers' names

--- src/main/java/com/reserv/reservation_system/ReservationController.java
+++ src/main/java/com/reserv/reservation_system/ReservationController.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/reservation")
 public class ReservationController {
    
     private static final Logger log = LoggerFactory.getLogger(ReservationController.class);
@@ -34,7 +36,7 @@ public class ReservationController {
                 .body(reservationService.getReservationById(id));
     }
 
-    @GetMapping("/getAll")
+    @GetMapping
     public ResponseEntity<List<Reservation>> getAllReservations() {
         log.info("Called getAllReservations()");
         return ResponseEntity.status(HttpStatus.OK)
