e7a3063e3e1a76ddb5f869d9e1c2354e197f0d69
martyny <qqmartyny@gmail.com>
2026-02-02 23:25:18 +0300

    refactor: make service return Reservation

--- src/main/java/com/reserv/reservation_system/ReservationService.java
+++ src/main/java/com/reserv/reservation_system/ReservationService.java
@@ -1,12 +1,21 @@
 package com.reserv.reservation_system;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReservationService {
 
-    public String getReservationById() {
-        return "it works";    
+    public Reservation getReservationById(Long id) {
+        return new Reservation(
+            id,
+            1L,
+            143L,
+            LocalDate.now(),
+            LocalDate.now().plusDays(7),
+            ReservationStatus.APPROVED
+        );    
     }
     
 }
