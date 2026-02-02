4163f5a47b6fe7e144c2bdd24b104e3ac58db9e2
martyny <qqmartyny@gmail.com>
2026-02-03 00:06:36 +0300

    feat(service): add storing of Reservations in a map

--- src/main/java/com/reserv/reservation_system/ReservationService.java
+++ src/main/java/com/reserv/reservation_system/ReservationService.java
@@ -2,40 +2,49 @@ package com.reserv.reservation_system;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReservationService {
 
-    public Reservation getReservationById(Long id) {
-        return new Reservation(
-            id,
+    private final Map<Long, Reservation> reservationMap = Map.of(
+        1L, new Reservation(
             1L,
+            12L,
             143L,
             LocalDate.now(),
             LocalDate.now().plusDays(7),
             ReservationStatus.APPROVED
-        );    
+        ), 
+        2L, new Reservation(
+            2L,
+            13L,
+            14123L,
+            LocalDate.now(),
+            LocalDate.now().plusDays(7),
+            ReservationStatus.APPROVED
+        ),
+        3L, new Reservation(
+            3L,
+            13L,
+            14343L,
+            LocalDate.now(),
+            LocalDate.now().plusDays(7),
+            ReservationStatus.APPROVED
+        ));
+
+    public Reservation getReservationById(Long id) {
+        if (!reservationMap.containsKey(id)) {
+            throw new NoSuchElementException("No Reservation with id: " + id);
+        }
+        return reservationMap.get(id);
     }
 
     public List<Reservation> findAllReservations() {
-        return List.of(new Reservation(
-            1L,
-            1L,
-            143L,
-            LocalDate.now(),
-            LocalDate.now().plusDays(7),
-            ReservationStatus.APPROVED
-        ), new Reservation(
-            2L,
-            1L,
-            143L,
-            LocalDate.now(),
-            LocalDate.now().plusDays(7),
-            ReservationStatus.APPROVED
-        )
-        );
+        return reservationMap.values().stream().toList();
     }
     
 }
