f1e0744eb542ca944dd254dbecbdad4eee44fb36
martyny <qqmartyny@gmail.com>
2026-02-06 00:39:44 +0300

    add validation exceptions handling for bad request

--- src/main/java/com/reserv/reservation_system/GlobalExceptionHandler.java
+++ src/main/java/com/reserv/reservation_system/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -46,7 +47,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(exception = {
         IllegalArgumentException.class,
-        IllegalStateException.class
+        IllegalStateException.class,
+        MethodArgumentNotValidException.class
     })
     public ResponseEntity<ErrorResponseDto> handleBadRequest(Exception e) {
         log.error("Handle BadRequest", e);
