5793beac67927624b2de921ef1866210fe0121ff
martyny <qqmartyny@gmail.com>
2026-02-08 01:53:59 +0300

    feat(repository): add pissimistic lock method

--- src/main/java/com/reserv/reservation_system/reservation/persistence/ReservationRepository.java
+++ src/main/java/com/reserv/reservation_system/reservation/persistence/ReservationRepository.java
@@ -5,34 +5,36 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.reserv.reservation_system.reservation.domain.Reservation;
 import com.reserv.reservation_system.reservation.domain.ReservationStatus;
+
+import jakarta.persistence.LockModeType;
 
 public interface ReservationRepository extends JpaRepository<ReservationEntity, Long> {
     
     @Modifying
-    @Query(
-        """
-            UPDATE ReservationEntity r
-            SET r.status = :status
-            WHERE r.id = :id
-        """)
+    @Query("""
+        UPDATE ReservationEntity r
+        SET r.status = :status
+        WHERE r.id = :id
+    """)
     void setStatus(
         @Param("id") Long id,
         @Param("status") ReservationStatus status
     );
 
-    @Query(
-        """
-            SELECT r.id from ReservationEntity r
-            WHERE r.roomId = :roomId
-            AND :startDate < r.endDate
-            AND r.startDate < :endDate
-            AND r.status = :status
-        """)
+    @Query("""
+        SELECT r.id from ReservationEntity r
+        WHERE r.roomId = :roomId
+        AND :startDate < r.endDate
+        AND r.startDate < :endDate
+        AND r.status = :status
+    """)
     List<Long> findConflictReservationIds(
         @Param("roomId") Long roomId,
         @Param("startDate") LocalDate starDate,
@@ -40,15 +42,29 @@ public interface ReservationRepository extends JpaRepository<ReservationEntity, 
         @Param("status") ReservationStatus status
     );
 
-    @Query(
-        """
-            SELECT r.id from ReservationEntity r
-            WHERE (:roomId IS NULL OR r.roomId = :roomId)
-            AND (:userId IS NULL OR r.userId = :userId)
-        """)
+    @Query("""
+        SELECT r.id from ReservationEntity r
+        WHERE (:roomId IS NULL OR r.roomId = :roomId)
+        AND (:userId IS NULL OR r.userId = :userId)
+    """)
     List<ReservationEntity> searchAllByFilter(
         @Param("roomId") Long roomId,
         @Param("userId") Long userId,
         Pageable pageable
+    );
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+        SELECT r FROM ReservationEntity r
+        WHERE r.roomId = :roomId
+        AND r.status = :status
+        AND :startDate < r.endDate
+        AND r.startDate < :endDate
+    """)
+    List<ReservationEntity> findAndLockConflictingReservations(
+        @Param("roomId") Long roomId,
+        @Param("startDate") LocalDate starDate,
+        @Param("endDate") LocalDate enDate,
+        @Param("status") ReservationStatus status
     );
 }
