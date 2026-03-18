2fb7f10145ce92f8b87cb75f87f8ca266390028d
martyny <qqmartyny@gmail.com>
2026-03-18 21:51:47 +0300

    fix(api): add status for response

--- src/main/java/com/reserv/reservation_system/reservation/api/dto/ReservationResponse.java
+++ src/main/java/com/reserv/reservation_system/reservation/api/dto/ReservationResponse.java
@@ -2,13 +2,16 @@ package com.reserv.reservation_system.reservation.api.dto;
 
 import java.time.LocalDate;
 
+import com.reserv.reservation_system.reservation.domain.ReservationStatus;
+
 
 public record ReservationResponse(
     Long id,
     Long userId,
     Long roomId,
     LocalDate startDate,
-    LocalDate endDate
+    LocalDate endDate,
+    ReservationStatus status
 ) {
     
 }
