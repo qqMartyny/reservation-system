220600835d76f338e9e0756bbef8c6b90803b843
martyny <qqmartyny@gmail.com>
2026-03-18 21:34:11 +0300

    fix(api): remove validation annotations from dto for response

--- src/main/java/com/reserv/reservation_system/reservation/api/dto/CreateReservationResponse.java
+++ src/main/java/com/reserv/reservation_system/reservation/api/dto/CreateReservationResponse.java
@@ -2,15 +2,13 @@ package com.reserv.reservation_system.reservation.api.dto;
 
 import java.time.LocalDate;
 
-import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotNull;
 
 public record CreateReservationResponse(
-    @NotNull Long id,
-    @NotNull Long userId,
-    @NotNull Long roomId,
-    @NotNull @FutureOrPresent LocalDate startDate,
-    @NotNull @FutureOrPresent LocalDate endDate
+    Long id,
+    Long userId,
+    Long roomId,
+    LocalDate startDate,
+    LocalDate endDate
 ) {
     
 }
