740cc75380ff3606974cfd5698e07946b9ef3e4b
martyny <qqmartyny@gmail.com>
2026-03-18 21:27:38 +0300

    feat(api): add DTO for reservation creation request

---
+++ src/main/java/com/reserv/reservation_system/reservation/api/dto/CreateReservationRequest.java
@@ -0,0 +1,15 @@
+package com.reserv.reservation_system.reservation.api.dto;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+
+public record CreateReservationRequest(
+    @NotNull Long userId,
+    @NotNull Long roomId,
+    @NotNull @FutureOrPresent LocalDate startDate,
+    @NotNull @FutureOrPresent LocalDate endDate
+) {
+    
+}
